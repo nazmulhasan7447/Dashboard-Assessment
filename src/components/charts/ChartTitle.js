0700ecc9caf6750d9c3ca5b964491367b10c0aeb
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2022-06-09 16:47:01 +0600

    updated title color

--- src/components/charts/ChartTitle.js
+++ src/components/charts/ChartTitle.js
@@ -1,9 +1,10 @@
 import React from "react";
+import '../../assets/css/chartTitle.css';
 
 const ChartTitle = ({title}) => {
     return (
         <React.Fragment>
-            <div className="">
+            <div className="chart_title">
                 <h3 className="mt-5">{title}</h3>
             </div>
         </React.Fragment>   
